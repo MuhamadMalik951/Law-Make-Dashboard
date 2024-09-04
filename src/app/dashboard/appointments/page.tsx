85cba9f72820bd3b79fc6c8eda18cb3437620ba7
MuhamadMalik951 <muhamadmalik471@gmail.com>
2024-09-04 13:20:54 +0500

    added pagination ui, yet to implement its function

--- src/app/dashboard/appointments/page.tsx
+++ src/app/dashboard/appointments/page.tsx
@@ -1,5 +1,6 @@
 'use-client';
 import { Table } from '@/app/ui/appointments/components';
+import Pagination from '@/app/ui/appointments/pagination';
 import MenuStat from '@/app/ui/appointments/statmenu';
 
 export default function page() {
@@ -12,6 +13,9 @@ export default function page() {
       </div>
       <div className="flex flex-col gap-6">
         <Table />
+      </div>
+      <div className="flex w-full justify-end mt-4">
+        <Pagination />
       </div>
     </div>
   );
---
+++ src/app/ui/appointments/pagination.tsx
@@ -0,0 +1,13 @@
+export default function Pagination() {
+  return (
+    // <div className="bg-white text-black">
+      <div className="join bg-white">
+        <button className="join-item bg-cyan-500 text-white btn btn-xs">1</button>
+        <button className="join-item bg btn btn-xs btn-active bg-white ">2</button>
+        <button className="join-item btn btn-xs bg-white">3</button>
+        <button className="join-item btn btn-xs bg-white">...</button>
+        <button className="join-item btn btn-xs bg-white">12</button>
+      </div>
+    // </div>
+  );
+}
