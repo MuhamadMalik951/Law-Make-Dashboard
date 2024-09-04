8823b7b196cd383f8930a370240988c2eca3bcb8
MuhamadMalik951 <muhamadmalik471@gmail.com>
2024-09-04 17:45:59 +0500

    made changes in header

--- src/app/ui/appointments/pagination.tsx
+++ src/app/ui/appointments/pagination.tsx
@@ -1,6 +1,5 @@
 export default function Pagination() {
   return (
-    // <div className="bg-white text-black">
       <div className="join bg-white">
         <button className="join-item bg-cyan-500 text-white btn btn-xs">1</button>
         <button className="join-item bg btn btn-xs btn-active bg-white ">2</button>
@@ -8,6 +7,5 @@ export default function Pagination() {
         <button className="join-item btn btn-xs bg-white">...</button>
         <button className="join-item btn btn-xs bg-white">12</button>
       </div>
-    // </div>
   );
 }
--- src/app/ui/dashboard/header.tsx
+++ src/app/ui/dashboard/header.tsx
@@ -3,37 +3,52 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <>
-      <div className="navbar h-8 bg-white text-black flex justify-between">
+      <div className="navbar  bg-white text-black flex justify-between">
         <div className="flex flex-col m-3">
           <div className="text-3xl font-extrabold">
             Law <span className="text-cyan-500">Mate</span>
           </div>
           <div className="text-xs font-bold">Your Parnter In Justice</div>
         </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end">
-            <div className="text-sm mb-3">Mr Hrais Irfan</div>
+        <div className="flex gap-3">
+          <div className="text flex  flex-col hidden md:flex">
+            <div className="font-bold text-xl">Mr Hrais Irfan</div>
+            <div className="text-gray-500 text-semibold text-sm text-right">
+              Bussiness Lawyer
+            </div>
           </div>
-          <div className="dropdown dropdown-end">
+          <div className="dropdown dropdown-end flex text-center">
             <div
               tabIndex={0}
               role="button"
-              className="btn btn-ghost btn-circle avatar"
+              className="btn btn-ghost btn-circle avatar w-16"
             >
-              <div className="w-12 rounded-full btn btn-circle avatar rounded-full rounded-box">
+              <div className="w-14 rounded-full">
                 <Image
-                  className="roundeed-full"
-                  width={500}
-                  height={500}
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                  alt="external-lawyer-male-profession-vitaliy-gorbachev-lineal-color-vitaly-gorbachev-1"
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  width={600}
+                  height={600}
                 />
               </div>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            ></ul>
+              className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-16 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge bg-white text-black">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
